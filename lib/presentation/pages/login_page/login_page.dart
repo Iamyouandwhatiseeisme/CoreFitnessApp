96ed57232b6fef5a9df1ada49fe21296fb380634
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2025-05-15 16:55:34 +0400

    feat(add): add google andgithub signup icons, style the login page to better suit the needs of user

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import "package:simple_icons/simple_icons.dart";
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -33,99 +34,185 @@ class _MyHomePageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 40,
-                          left: 10,
-                          right: 10,
-                        ),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.blueGrey.withAlpha(10),
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.blueGrey.withAlpha(10),
-                                width: 2,
-                              ),
-                            ),
-                          ),
-                          obscureText: true,
-                        ),
-                      ),
-                      SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: BoxBorder.all(
+                      color: Colors.grey.withAlpha(80),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
 
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10, left: 20.0),
-                            child: Text(
-                              "Forgot Password",
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            left: 10,
+                            right: 10,
+                          ),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.blueGrey.withAlpha(40),
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
                               ),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ElevatedButton(
+                        ),
+                        SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.blueGrey.withAlpha(40),
+                                  width: 2,
+                                ),
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                top: 10,
+                                left: 20.0,
+                              ),
+                              child: Text(
+                                "Forgot Password",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 5,
+                                  foregroundColor: Colors.white,
+                                  shadowColor: Colors.grey.withAlpha(70),
+                                  backgroundColor: Colors.blue.withAlpha(80),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 20,
+                                    vertical: 10,
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  'Log in',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account?"),
+                            TextButton(
                               onPressed: () {},
-                              child: Text('Login'),
+                              child: Text('Sign Up'),
                             ),
+                          ],
+                        ),
+                        Divider(
+                          color: Colors.grey.withAlpha(50),
+                          height: 20,
+                          thickness: 1,
+                          indent: 20,
+                          endIndent: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [Text("Or login with")],
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              foregroundColor: Colors.white,
+                              shadowColor: Colors.grey.withAlpha(70),
+                              backgroundColor: Colors.grey.withAlpha(80),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10,
+                              ),
+                            ),
+                            onPressed: () {},
+                            label: Text(
+                              "Sign in with Google",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black.withAlpha(200),
+                              ),
+                            ),
+                            // icon: Icon(SimpleIcons.google),
+                            icon: SvgPicture.asset('assets/icons/google.svg'),
                           ),
-                        ],
-                      ),
-
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account?"),
-                          TextButton(onPressed: () {}, child: Text('Sign Up')),
-                        ],
-                      ),
-                      Divider(
-                        color: Colors.grey.withAlpha(50),
-                        height: 20,
-                        thickness: 1,
-                        indent: 20,
-                        endIndent: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Text("Or login with")],
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: () {},
-                        label: Text("Google"),
-                        icon: SvgPicture.asset('assets/icons/google.svg'),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: () {},
-                        label: Text("Github"),
-                        icon: SvgPicture.asset('assets/icons/github.svg'),
-                      ),
-                    ],
+                        ),
+                        SizedBox(height: 10),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              foregroundColor: Colors.white,
+                              shadowColor: Colors.grey.withAlpha(70),
+                              backgroundColor: Colors.grey.withAlpha(80),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10,
+                              ),
+                            ),
+                            onPressed: () {},
+                            label: Text(
+                              "Sign in with Github",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black.withAlpha(200),
+                              ),
+                            ),
+                            icon: SvgPicture.asset('assets/icons/github.svg'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
